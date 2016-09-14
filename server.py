ec7329287f837173d3bc5db9dfc73b31b321e7c7
guyao <gamesgao@163.com>
2016-09-14 22:03:10 +0800

    基本完成大致功能，虽然有些问题。。。

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 
 import sys
 from PyQt4 import QtGui, QtCore
+import sip
 
 class Example(QtGui.QWidget):
 	
@@ -10,6 +11,7 @@ class Example(QtGui.QWidget):
 		self.label = [QtGui.QLabel(self)]
 		self.labelNum = 0
 		self.temp = [0,0]
+
 		self.initUI()
 	def initUI(self):
 		#self.setGeometry(300, 300, 280, 170)
@@ -34,17 +36,31 @@ class Example(QtGui.QWidget):
 
 		self.timer=QtCore.QTimer()
 		QtCore.QObject.connect(self.timer,QtCore.SIGNAL("timeout()"), self.OnTimer)
-		self.timer.start( 1000 )
+		self.timer.start( 2000 )
 		self.qp = QtGui.QPainter()
 		self.adjustSize()
 		self.show()
+
 	def OnTimer(self):
-		print 'Get Timer'
 		self.timer.stop()
 
+		temp = len(self.label)-2
+		for i in self.label[:0:-1]:
+			i.setGeometry(self.label[temp].x(), self.label[temp].y(), i.width(), i.height())
+			temp -= 1
+		temp = 0
+		sip.delete(self.label[0])
+		for i in self.label[1:]:
+			self.label[temp] = i
+			temp += 1
 
-		self.adjustSize()
-		self.show()
+		del self.label[len(self.label)-1]
+		print self.label
+		self.labelNum -= 1
+		if len(self.label) != 0:
+			self.timer.start( 2000 )
+
+
 	def paintEvent(self, e):
 		
 		self.qp.begin(self)
@@ -56,9 +72,15 @@ class Example(QtGui.QWidget):
 		# self.qp.fillRect(self.label[0].x(),self.label[0].y()-5,self.label[0].width(),self.label[0].height()+10, QtGui.QColor(0,0,0,169))
 		self.qp.end()
 
+
 	def keyPressEvent(self, e):
+
 	 	if e.key() == QtCore.Qt.Key_Escape:
 			self.close()
+		else:
+			if e.key() == QtCore.Qt.Key_A:
+				self.insertMessage("test")
+
 
 	def mousePressEvent(self, e):
 		self.temp = [e.x(), e.y()]
@@ -71,7 +93,10 @@ class Example(QtGui.QWidget):
 		tempLabel = QtGui.QLabel(self)
 		tempLabel.setText(str)
 		tempLabel.setFont(QtGui.QFont("Microsoft Yahei",50,QtGui.QFont.Bold))
-		tempLabel.setGeometry(self.label[self.labelNum-1].x(), self.label[self.labelNum-1].y()+self.label[self.labelNum-1].height(),self.label[self.labelNum-1].width(),self.label[self.labelNum-1].height())
+		if self.labelNum != 0:
+			tempLabel.setGeometry(self.label[self.labelNum-1].x(), self.label[self.labelNum-1].y()+self.label[self.labelNum-1].height(),self.label[self.labelNum-1].width(),self.label[self.labelNum-1].height())
+		else:
+			self.timer.start( 2000 )
 		# 自动调整大小
 		tempLabel.adjustSize()
 		tempLabel.setAlignment(QtCore.Qt.AlignCenter)
@@ -79,16 +104,16 @@ class Example(QtGui.QWidget):
 		pe = QtGui.QPalette() 
 		pe.setColor(QtGui.QPalette.WindowText,QtGui.QColor(255,255,255,255))#设置字体颜色 
 		tempLabel.setPalette(pe)
+
 		tempLabel.show()
 
 		self.labelNum += 1
 		self.label += [tempLabel]
 
 		self.adjustSize()
-		self.show()
 
 app = QtGui.QApplication(sys.argv)
 trans = Example()
-trans.insertMessage("str")
-trans.insertMessage("wwww")
+
+
 sys.exit(app.exec_())
